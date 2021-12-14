5ff0c874c36305d652d69ddd4d37863edf911af4
Dong Min <dongmin@pddoctor.cn>
2021-12-14 10:50:29 +0800

    feat: button 组件

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,10 @@
+import { RefButton } from './Button';
+
 export { default as Modal } from './Modal';
 export { default as Tooltip } from './Tooltip';
 export { default as Popover } from './Popover';
 export { default as Message } from './Message';
 export { default as Tag } from './Tag';
 export { default as Countdown } from './Countdown';
+export { default as Button } from './Button';
+export { RefButton };
